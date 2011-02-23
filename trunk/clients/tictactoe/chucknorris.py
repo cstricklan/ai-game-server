f7b672703a2d05e257307ae47cd23c3c3ba306fc
JamesDavidThompson@gmail.com <JamesDavidThompson@gmail.com@91086bde-53e9-c652-f415-fcbad8bd8235>
2011-02-23 13:43:51 +0000

    More special state handling

--- trunk/clients/tictactoe/chucknorris.py
+++ trunk/clients/tictactoe/chucknorris.py
@@ -40,7 +40,7 @@ def nextMoveWin(board):
 def checkSpecial(board):
     if player == 1:
         if (board == "X---O---X") or (board == "--X-O-X--"): return 1
-        if (board == "-X--OX---"): return 2
+        if (board == "-X--OX---"): return 1
         if (board == "-X-XO----"): return 0
         if (board == "----OX-X-"): return 8
         if (board == "---XO--X-"): return 6
